b595d1569384ed98a0bd3dd5eb17d8e299ea8cb3
Trenton Kress <trenton.kress@gmail.com>
2017-02-09 20:49:45 -0800

    Fix script load ordering so the canvas is there before the script initializes.

--- app/engine/render/renderer.js
+++ app/engine/render/renderer.js
@@ -6,13 +6,18 @@ Renderer.prototype.initGL = function() {
   this.canvas = document.getElementById('game-canvas');
 
   try {
-    this.gl = canvas.getContext('experimental-webgl');
+    this.gl = this.canvas.getContext('experimental-webgl');
   } catch(e) {
     console.log(e);
   }
 
   if (!this.gl) {
     console.log('WebGL unsupported.');
+  } else {
+    this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    this.gl.clearDepth(1.0);
+    this.gl.enable(this.gl.DEPTH_TEST);
+    this.gl.depthFunc(this.gl.LEQUAL);
   }
 };
 
